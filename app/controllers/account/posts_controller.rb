dcd7f759d1096f3a917f97a4c65457639136405d
Corner <446042055@qq.com>
2021-01-29 21:21:56 +0800

    eidt post and delete post

--- app/controllers/account/posts_controller.rb
+++ app/controllers/account/posts_controller.rb
@@ -1,6 +1,34 @@
 class Account::PostsController < ApplicationController
   before_action :authenticate_user!
+  before_action :find_post, only: [:edit, :update, :destroy]
   def index
     @posts = current_user.posts
+  end
+
+  def edit
+    
+  end
+
+  def update
+    if @post.update(post_params)
+      redirect_to account_posts_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @post.destroy
+    redirect_to account_posts_path
+  end
+
+  private
+  
+  def find_post
+    @post = current_user.posts.find(params[:id])
+  end
+
+  def post_params
+    params.require(:post).permit(:content)
   end
 end
